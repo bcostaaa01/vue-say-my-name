978dd52d389cc5df09c81c437efc35d23dd5d8d3
bcostaaa01 <bcostinha01@gmail.com>
2022-08-03 23:09:57 +0100

    Render text and myName variable in seperate <p> element

--- src/components/SayMyName.vue
+++ src/components/SayMyName.vue
@@ -14,7 +14,8 @@ function sayName () {
   <img src="https://i.pinimg.com/originals/e4/26/70/e426702edf874b181aced1e2fa5c6cde.gif" alt="">
 
   <div class="card">
-    <button type="button" @click="sayName">My name is {{ myName }}</button>
+    <button type="button" @click="sayName">Display your name</button>
+    <p>My name is {{ myName }}</p>
     </div>
     </template>
 
